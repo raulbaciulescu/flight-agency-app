cca17dd86d579393a07a2884ca2ef845da44830f
raulbaciulescu <raulbaciulescu@gmail.com>
2022-03-08 01:43:29 +0200

    domain and repo interface c#

---
+++ travelAgency2/travelAgency2/Domain/Purchase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travelAgency2.src.Domain
+{
+    internal class Purchase : Entity<long>
+    {
+
+        public Flight flight { get; set; }
+        public string clientName { get; set; }
+        public string clientAddress { get; set; }
+        public List<string> tourists { get; set; }
+        public int nrOfSeats { get; set; }
+
+        public Purchase(long id, Flight flight, string clientName, string clientAddress, List<string> tourists, int nrOfSeats) : base(id)
+        {
+            this.flight = flight;
+            this.clientName = clientName;
+            this.clientAddress = clientAddress;
+            this.tourists = tourists;
+            this.nrOfSeats = nrOfSeats; 
+        }
+    }
+}
