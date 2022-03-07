e1814d71bfe8abf2fdbe5db5db0cfa0e8e151ca4
raulbaciulescu <raulbaciulescu@gmail.com>
2022-03-08 01:43:29 +0200

    domain and repo interface c#

---
+++ travelAgency2/travelAgency2/Repository/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using travelAgency2.src.Domain;
+
+namespace travelAgency2.src.Repository
+{
+    internal interface Repository<ID, T> where T : Entity<ID>
+    {
+        void add(T entity);
+        void update(T entity);
+        T findByID(ID id);
+        void delete(ID id);
+        List<T> getAll();
+    }
+}
