1e72b96e71e4261822cb488a419f8b96e6c85864
raulbaciulescu <raulbaciulescu@gmail.com>
2022-03-18 18:17:09 +0200

    some changes

--- travelAgency/src/main/java/group/travelagency/HelloApplication.java
+++ travelAgency/src/main/java/group/travelagency/HelloApplication.java
@@ -33,23 +33,23 @@ public class HelloApplication extends Application {
         Purchase purchase = new Purchase(flight, "Client1", "strada garofitei",
                 tourists, 4);
         purchase.setId(1L);
-//        Resources.getInstance().getLocationRepository().add(location);
-//        Resources.getInstance().getLocationRepository().getAll().forEach(System.out::println);
-//        System.out.println("\n");
+        Resources.getInstance().getLocationRepository().add(location);
+        Resources.getInstance().getLocationRepository().getAll().forEach(System.out::println);
+        System.out.println("\n");
 
-//        Resources.getInstance().getFlightRepository().add(flight);
-//        Resources.getInstance().getFlightRepository().getAll().forEach(System.out::println);
-//        System.out.println("\n");
+        Resources.getInstance().getFlightRepository().add(flight);
+        Resources.getInstance().getFlightRepository().getAll().forEach(System.out::println);
+        System.out.println("\n");
 
         Resources.getInstance().getPurchaseRepository().add(purchase);
         Resources.getInstance().getPurchaseRepository().getAll().forEach(System.out::println);
 
-//        User user1 = new User("raul", "pass",
-//                "raul", "baciulescu");
-//        User user2 = new User("maria", "sspass",
-//                "raul", "baciulescuss");
-//        user1.setId(1L);
-//        user2.setId(2L);
+        User user1 = new User("raul", "pass",
+                "raul", "baciulescu");
+        User user2 = new User("ra", "sspass",
+                "raul", "baciulescuss");
+        user1.setId(1L);
+        user2.setId(2L);
 //
 //        UserRepository repo = Resources.getInstance().getUserRepository();
 //        repo.add(user1);
