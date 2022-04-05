fa6150c297cefcda81e65a4ec4de93d53bdedd34
raulbaciulescu <raulbaciulescu@gmail.com>
2022-04-05 09:36:07 +0300

    networking in java

---
+++ travelAgency/Persistence/src/main/java/impl/database/TableFactory.java
@@ -0,0 +1,33 @@
+package impl.database;
+
+import api.Constants;
+import api.Table;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class TableFactory {
+    private final JdbcUtils dbUtils;
+
+    public TableFactory() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(Constants.Db.filename));
+        } catch (IOException e) {
+            System.out.println("Cannot find bd.config " + e);
+        }
+        dbUtils = new JdbcUtils(properties);
+    }
+
+    public Table<?, ?> getTable(Constants.Db.Tables table) throws SQLException {
+        if (table == Constants.Db.Tables.USER)
+            return new UserTable(dbUtils.getConnection());
+        if (table == Constants.Db.Tables.FLIGHT)
+            return new FlightTable(dbUtils.getConnection());
+        if (table == Constants.Db.Tables.PURCHASE)
+            return new PurchaseTable(dbUtils.getConnection());
+        return new LocationTable(dbUtils.getConnection());
+    }
+}
