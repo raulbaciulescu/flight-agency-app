1740a05cf60055d7e013c2b7a090bf6b07c26ee2
raulbaciulescu <raulbaciulescu@gmail.com>
2022-04-05 09:36:07 +0300

    networking in java

---
+++ travelAgency/Model/src/main/java/domain/User.java
@@ -0,0 +1,73 @@
+package domain;
+
+public class User extends Entity<Long>{
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public User(Long id, String password) {
+        this.setId(id);
+    }
+
+    public User(Long id) {
+        this.setId(id);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
